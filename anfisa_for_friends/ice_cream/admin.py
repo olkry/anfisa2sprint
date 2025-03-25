104ef917386e2e4d694c0ad8c79644c3a727c8ec
Oleg Kryuchikhin <adenos90@gmail.com>
2025-03-25 18:36:57 +0300

    Add DB and admin

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Topping, Wrapper, IceCream
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    # Указываем, для каких связанных моделей нужно включить такой интерфейс:
+    filter_horizontal = ('toppings',)
+
+
+# Подготавливаем модель IceCream для вставки на страницу другой модели.
+class IceCreamInline(admin.TabularInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+# # Этот вариант сработает для всех моделей приложения.
+# # Вместо пустого значения в админке будет отображена строка "Не задано".
+# admin.site.empty_value_display = 'Не задано'
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+# Регистрируем кастомное представление админ-зоны
+admin.site.register(IceCream, IceCreamAdmin)
